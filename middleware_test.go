b30196aa53398b9acd3c074626bd7bfa6307588d
Andrew <kolbe1129@gmail.com>
2021-06-10 15:12:51 -0500

    added No Surf and test

--- middleware_test.go
+++ middleware_test.go
@@ -1,2 +1,24 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+	"testing"
+)
+
+
+func TestNoSurf(t *testing.T) {
+	// we have to pass in a handler to no surf so it will hand back a handler
+
+	var myH myHandler // type myHandler is defined in setup_test.go
+
+	h := NoSurf(&myH) // we have to pass a http.Handler into NoSurf for it to run
+
+	// test that No Surf takes in a handler and returns a handler
+	switch v := h.(type) {
+	case http.Handler:
+		// do nothing. this is what we expect
+	default:
+		t.Error(fmt.Sprintf("type is not http.Handler, but is  %T", v))
+	}
+}
--- setup_test.go
+++ setup_test.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	// "net/http"
+	"net/http"
 	"os"
 	"testing"
 )
@@ -11,8 +11,8 @@ func TestMain(m *testing.M) { // there must be a func called TestMain in the set
 	os.Exit(m.Run()) // exit all of the tests when they are finished
 }
 
-// // set up an http.Handler that we can use in our testing envirnment only
-// type myHandler struct{}
-// func (mh *myHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+// set up an http.Handler that we can use in our testing envirnment only
+type myHandler struct{}
+func (mh *myHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
-// }
+}
