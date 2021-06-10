003f356608e4b4703ae24882ec784e7eed9a44b6
Andrew <kolbe1129@gmail.com>
2021-06-10 15:37:51 -0500

    changed name of helpers package to render

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -4,25 +4,25 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/andkolbe/go-websockets/internal/helpers"
+	"github.com/andkolbe/go-websockets/internal/render"
 )
 
 func Login(w http.ResponseWriter, r *http.Request) {
-	err := helpers.RenderPage(w, r, "login.jet.html", nil)
+	err := render.RenderPage(w, r, "login.jet.html", nil)
 	if err != nil {
 		log.Println(err)
 	}
 }
 
 func Register(w http.ResponseWriter, r *http.Request) {
-	err := helpers.RenderPage(w, r, "register.jet.html", nil)
+	err := render.RenderPage(w, r, "register.jet.html", nil)
 	if err != nil {
 		log.Println(err)
 	}
 }
 
 func Chat(w http.ResponseWriter, r *http.Request) {
-	err := helpers.RenderPage(w, r, "chat.jet.html", nil)
+	err := render.RenderPage(w, r, "chat.jet.html", nil)
 	if err != nil {
 		log.Println(err)
 	}
--- internal/render/render.go
+++ internal/render/render.go
@@ -1,4 +1,4 @@
-package helpers
+package render
 
 import (
 	"log"
