3bf7dafce29f480a92d71945d50bc182323e1039
Andrew <kolbe1129@gmail.com>
2021-06-11 01:11:19 -0500

    Added Users model

--- internal/models/user.go
+++ internal/models/user.go
@@ -1,18 +1,21 @@
 package models
 
-import "gorm.io/gorm"
+import "time"
 
+// we have to describe our database in a way that Go understands
+// every table gets a model
 type User struct {
-	gorm.Model
-	UserName  string `json:"username"` // format the JSON response to look like these values instead
-	FirstName string `json:"first_name"`
-	LastName  string `json:"last_name"`
-	Email     string `json:"email" gorm:"unique"`
-	Password  []byte `json:"-"` // we don't want to send the password to the client
+	ID        int
+	FirstName string
+	LastName  string
+	Username  string
+	Email     string
+	Password  []byte
+	CreatedAt time.Time
 }
 
 /*
-The primitive data types prefixed with "u" are unsigned versions with the same bit sizes. 
-Effectively, this means they cannot store negative numbers, but on the other hand they can store positive numbers twice as large as their signed counterparts. 
+The primitive data types prefixed with "u" are unsigned versions with the same bit sizes.
+Effectively, this means they cannot store negative numbers, but on the other hand they can store positive numbers twice as large as their signed counterparts.
 The signed counterparts do not have "u" prefixed.
 */
