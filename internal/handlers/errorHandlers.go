7745eda384a957c818ab92e7b3124edfcf547401
Andrew <kolbe1129@gmail.com>
2021-06-15 15:52:55 -0500

    moved handler errors to their own file

---
+++ internal/handlers/errorHandlers.go
@@ -0,0 +1,46 @@
+package handlers
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"runtime/debug"
+)
+
+
+func printTemplateError(w http.ResponseWriter, err error) {
+	_, _ = fmt.Fprintf(w, `<small><span class='text-danger'>Error executing template: %s</span></small>`, err)
+}
+
+// ClientError will display error page for client error i.e. bad request
+func ClientError(w http.ResponseWriter, r *http.Request, status int) {
+	switch status {
+	case http.StatusNotFound:
+		show404(w, r)
+	case http.StatusInternalServerError:
+		show500(w, r)
+	default:
+		http.Error(w, http.StatusText(status), status)
+	}
+}
+
+// ServerError will display error page for internal server error
+func ServerError(w http.ResponseWriter, r *http.Request, err error) {
+	trace := fmt.Sprintf("%s\n%s", err.Error(), debug.Stack())
+	_ = log.Output(2, trace)
+	show500(w, r)
+}
+
+func show404(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusNotFound)
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	w.Header().Set("Cache-Control", "no-store, no-cache, must-revalidate, post-check=0, pre-check=0")
+	http.ServeFile(w, r, "./ui/static/404.html")
+}
+
+func show500(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusInternalServerError)
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	w.Header().Set("Cache-Control", "no-store, no-cache, must-revalidate, post-check=0, pre-check=0")
+	http.ServeFile(w, r, "./ui/static/500.html")
+}
